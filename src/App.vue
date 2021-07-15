d11d0f9ba5c45605fda325f6297cdc1ecd2c1463
Sardor Mamadaliyev <sardor.mamadaliyev@gmail.com>
2021-07-15 22:26:23 +0900

    :heavy_plus_sign: Add web3/tailwind/axios

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
   <div>
-    Explorer
+    <!-- <Navigation/> -->
+    <router-view/>
   </div>
 </template>
 
 <script>
-
+// import Navigation from '@/components/navigation.vue'
 export default {
-  name: 'App',
-  components: {
+  components:{
+    // Navigation
   }
 }
 </script>
 
 <style>
+
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
+import './plugins/axios'
 import App from './App.vue'
+import router from './router'
+import Web3 from 'web3'
+import './assets/css/tailwind.css';
+
+const provider = 'https://mainnet.infura.io/v3/1d6df4156b1a4080b7a8c1e3dafe44fc'
+const HttpProvider = new Web3.providers.HttpProvider(provider)
+const web3 = new Web3(HttpProvider)
+console.log(Web3)
+Vue.prototype.$web3 = web3
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
